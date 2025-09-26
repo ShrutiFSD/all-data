0c7d4a1de3845a40413cb5015e423fdecc5b25c5
ShrutiFSD <spbagde007@gmail.com>
2025-09-26 09:39:46 +0530

    Add files via upload

---
+++ javascript/productData.js
@@ -0,0 +1,28 @@
+productData = [
+    {
+        name:"Laptop",
+        price: 40000,
+        image:"https://wallpapers.com/images/featured/laptop-murjp1nk4lp1idlt.jpg",
+    },
+     {
+        name:"Smart TV",
+        price:35000,
+        image:"",
+     },
+     {
+        name:" Smartphone",
+        price:30000,
+        image:"",
+     },
+     {
+        name:"printer",
+        price:17000,
+        image:"",
+     },
+     {
+        name:"Smart AC",
+        price:60000,
+        image:"",
+     },
+     
+]
