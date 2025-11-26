4366e52e68fae38d253a37a64f56c4d97766f66d
ShrutiFSD <spbagde007@gmail.com>
2025-11-26 09:34:41 +0530

    Add files via upload

---
+++ c/checkvovels.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+int main(){
+    char str[100];
+    int count = 0, i = 0;
+
+    printf("enter a string :");
+    fgets(str,100,stdin);
+
+    for(i = 0; i < strlen(str)-1; i++){
+        if((str[i]== 'a'))
+    }
+
+}
---
+++ py/python/arrayOperation/class-eleven.py
@@ -0,0 +1,28 @@
+
+# x = int(input("Enter a number: "))
+# y = int(input("Enter a number: "))
+
+# sum = 0
+
+# for i in range(1, y + 1):
+#     sum +=  (x / i)
+
+# print(sum)
+
+
+x = int(input("Enter a num:"))
+sum = 0
+
+for i in range(1, x + 1):
+     sum += i / (i + 1)
+
+print(sum)
+
+# n = int(input("Enter n: "))
+
+# total = 0
+
+# for i in range(1, n + 1):
+#     total += i / (i + 1)
+
+# print("Sum =", total)
